715c5b3ad22beb4a8f14709d99d47a3ca31204a6
Brandon Ore <y2ktheory@gmail.com>
2018-05-03 23:13:44 -0600

    New cmd handler to read directories recursively. Organized cmds

--- index.js
+++ index.js
@@ -1,39 +1,52 @@
 const botconfig = require('./botconfig.json');
 const Discord = require('discord.js');
 const fs = require('fs');
+const { promisify } = require('util');
+const readdir = promisify(fs.readdir);
+const dir = require('node-dir');
 
+// assign new discord client
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
 
-fs.readdir('./commands/', (err, files) => {
-    if(err) console.log(err);
+// command handler, search directories recursively and set commands to discord collection
+const loadCommands = async() => {
+    const folders = await readdir('./commands/');
+    for(let i = 0; i < folders.length; i++) {
+        let folder = folders[i];
+        let files = await readdir(`./commands/${folder}/`);
+        for(let x = 0; x < files.length; x++) {
+            let file = files[x];
+            let props = require(`./commands/${folder}/${file}`);
+            if(!props) return;
+            if(!props.help.name) props.help.name = file.split('.')[1];
+            bot.commands.set(props.help.name, props);
+            console.log(`Command loaded: ${props.help.name}`);
+        }
+    }
+}
 
-    let jsFile = files.filter(f => f.split('.').pop() === 'js');
-    if(jsFile.length <= 0) console.log("Couldn't find any commands.");
-
-    jsFile.forEach((f, i) => {
-        let props = require(`./commands/${f}`);
-        console.log(`${f} loaded.`);
-        bot.commands.set(props.help.name, props);
-    });
-});
-
+// start/ready bot
 bot.on('ready', async() => {
     console.log(`${bot.user.username} is online`);
     bot.user.setActivity('Anime', { type: 'WATCHING' });
 });
 
+// listen for messages from any channel
 bot.on('message', async(message) => {
+    // ignore other bots and dm channels
     if(message.author.bot) return;
     if(message.channel.type === 'dm') return;
 
+    // setup command prefix and args based off message content
     let prefix = botconfig.prefix;
     let messageArray = message.content.split(' ');
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
-
     let commandFile = bot.commands.get(cmd.slice(prefix.length));
+
     if(commandFile) commandFile.run(bot, message, args);
 });
 
 bot.login(botconfig.token);
+loadCommands();
